72e1d9f65fce321f6ea47e40c15319b87c3b25e3
Dang Quoc Huy <huysamdua@yahoo.com>
2018-08-07 21:43:31 +0700

    replace many unsafe forces

--- ProcrastinatorsRevenge/ViewController.swift
+++ ProcrastinatorsRevenge/ViewController.swift
@@ -32,14 +32,19 @@ class ViewController: UIViewController {
   @IBOutlet weak var topMarginConstraint: NSLayoutConstraint!
   @IBOutlet var enterButtonArray: [UIButton]!
   
+  let showDirection = "show_directions"
+  
   var originalTopMargin: CGFloat!
   
   let locationManager = CLLocationManager()
-  var locationTuples: [(textField: UITextField, mapItem: MKMapItem?)]!
+  var locationTuples: [(textField: UITextField, mapItem: MKMapItem?)] = []
   
   var locationsArray: [(textField: UITextField, mapItem: MKMapItem?)] {
     var filtered = locationTuples.filter({ $0.mapItem != nil })
-    filtered += [filtered.first!]
+    if let firstItem = filtered.first {
+      filtered += [firstItem]
+    }
+    
     return filtered
   }
   
@@ -63,23 +68,23 @@ class ViewController: UIViewController {
   }
   
   override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-    if locationTuples[0].mapItem == nil ||
-      (locationTuples[1].mapItem == nil && locationTuples[2].mapItem == nil) {
-      showAlert("Please enter a valid starting point and at least one destination.")
-      return false
-    } else {
-      return true
-    }
+    return validRoute()
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    let directionsViewController = segue.destination as! DirectionsViewController
-    directionsViewController.locationArray = locationsArray
+    if segue.identifier == showDirection {
+      if let directionsViewController = segue.destination as? DirectionsViewController {
+        directionsViewController.locationArray = locationsArray
+      }
+    }
   }
   
   @IBAction func getDirections(_ sender: AnyObject) {
     view.endEditing(true)
-    performSegue(withIdentifier: "show_directions", sender: self)
+    
+    if validRoute() {
+      performSegue(withIdentifier: showDirection, sender: self)
+    }
   }
 
   @IBAction func addressEntered(_ sender: UIButton) {
@@ -103,7 +108,7 @@ class ViewController: UIViewController {
 
   @IBAction func swapFields(_ sender: AnyObject) {
     swap(&destinationField1.text, &destinationField2.text)
-    swap(&locationTuples[1].mapItem, &locationTuples[2].mapItem)
+    ((locationTuples[1].mapItem), (locationTuples[2].mapItem)) = ((locationTuples[2].mapItem), (locationTuples[1].mapItem))
     swap(&self.enterButtonArray.filter{$0.tag == 2}.first!.isSelected, &self.enterButtonArray.filter{$0.tag == 3}.first!.isSelected)
   }
   
@@ -117,8 +122,7 @@ class ViewController: UIViewController {
   }
   
   func formatAddressFromPlacemark(placemark: CLPlacemark) -> String {
-    return (placemark.addressDictionary!["FormattedAddressLines"] as!
-      [String]).joined(separator: ", ")
+    return (placemark.addressDictionary?["FormattedAddressLines"] as? [String] ?? [""]).joined(separator: ", ")
   }
   
   func showAddressTable(addresses: [String], textField: UITextField,
@@ -133,7 +137,18 @@ class ViewController: UIViewController {
     addressTableView.sender = sender
     addressTableView.delegate = addressTableView
     addressTableView.dataSource = addressTableView
+    
     view.addSubview(addressTableView)
+  }
+  
+  private func validRoute() -> Bool {
+    if locationTuples[0].mapItem == nil ||
+      (locationTuples[1].mapItem == nil && locationTuples[2].mapItem == nil) {
+      showAlert("Please enter a valid starting point and at least one destination.")
+      return false
+    } else {
+      return true
+    }
   }
   
   // The remaining methods handle the keyboard resignation/
@@ -165,7 +180,7 @@ class ViewController: UIViewController {
 extension ViewController: UITextFieldDelegate {
   
   func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-    enterButtonArray.filter{$0.tag == textField.tag}.first!.isSelected = false
+    enterButtonArray.filter{$0.tag == textField.tag}.first?.isSelected = false
     locationTuples[textField.tag-1].mapItem = nil
     return true
   }
@@ -189,12 +204,15 @@ extension ViewController: CLLocationManagerDelegate {
   
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     
-    CLGeocoder().reverseGeocodeLocation(locations.last!) { (placemarks, error) in
+    guard let location = locations.last else { return }
+    CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
       if let placemarks = placemarks {
+        
         let placemark = placemarks[0]
-        self.locationTuples[0].mapItem = MKMapItem(placemark:
-          MKPlacemark(coordinate: placemark.location!.coordinate,
-                      addressDictionary: placemark.addressDictionary as! [String:AnyObject]?))
+        guard let coordinate = placemark.location?.coordinate else { return }
+        guard let addressDictionary = placemark.addressDictionary as? [String:AnyObject]? else { return }
+        
+        self.locationTuples[0].mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coordinate, addressDictionary: addressDictionary))
         self.sourceField.text = self.formatAddressFromPlacemark(placemark: placemark)
         self.enterButtonArray.filter{$0.tag == 1}.first!.isSelected = true
       }
